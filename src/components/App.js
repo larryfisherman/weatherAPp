6c7433b59c61267311850658776823d2fe5c3b0d
larryfisherman <szczepannowak87@gmail.com>
2021-03-22 14:28:25 +0100

    Changed mobile layout

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ const App = () => {
   return (
     <div className={toggleClass ? "activeApp" : "unActiveApp"}>
       <div className={toggleClass ? "activeHeader" : "unActiveHeader"}>
+        <h1 className={toggleClass ? "active" : "unActive"}> Weather App </h1>
         <SearchingBar shareToggleClass={shareToggleClass} />
       </div>
       <div className={toggleClass ? "activeMain" : "unActiveMain"}>
